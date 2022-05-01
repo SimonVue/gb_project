0ab2411f9a84ad25f46631ad55541b0115d0e313
SimonVue <viktorov_@mail.ru>
2022-05-01 20:08:50 +0300

    feat. style.css

--- Lesson1/project/js/main.js
+++ Lesson1/project/js/main.js
@@ -1,23 +1,22 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
+    {id: 1, title: 'Notebook', price: 40000},
+    {id: 2, title: 'Mouse', price: 1000},
+    {id: 3, title: 'Keyboard', price: 2500},
+    {id: 4, title: 'Gamepad', price: 1500},
 ];
 
-const getProductHTMLString = (title = 'choosed device', price = 0) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+const renderProduct= (item, img='img/noimg.jpg') => `<div class="product-item" data-id="${this.id}">
+                <img src="${img}" alt="img">
+                <h3>${item.title}</h3>
+                <p>${item.price} \u20bd</p>
                 <button class="by-btn">Добавить</button>
               </div>`;
+
+
+const renderProducts = list => {
+  document
+    .querySelector('.products')
+    .insertAdjacentHTML('beforeend', list.map(item =>renderProduct(item)).join(''));
 };
-
-const renderProducts = (productList) => {
-  const list = productList.map((good) => getProductHTMLString(good.title, good.price));
-
-  document.querySelector('.products').innerHTML = list.join('');
-    // console.log(list);
-}
 
 renderProducts(products);
